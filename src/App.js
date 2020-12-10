492d16c18744b6892b8e4919fdc55d8a0f7bf465
Kalyan-2707 <52063829+Kalyan-2707@users.noreply.github.com>
2020-12-10 12:50:15 +0530

    Removed Warnings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect,useRef} from 'react';
+import React,{useState,useEffect} from 'react';
 import {Fetch} from './Components/Fetch';
 import Quiz from './Components/Quiz';
 import Nav from './Components/Nav';
