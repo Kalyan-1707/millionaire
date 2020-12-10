dd03a0b23628234b6b7e30c9528ab667b0463e90
Kalyan-2707 <52063829+Kalyan-2707@users.noreply.github.com>
2020-12-10 22:51:44 +0530

    logo added

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Nav({toggleMenu}) {
     return (
         <nav>
-            <span className="brand">Millionaire</span>
+            <span className="brand"><img src="./assets/millionaire-logo-dark-orange.png" width="150" height="50" alt="logo" /></span>
             <button className="toggle" onClick={() => toggleMenu()}>
                 <span className="material-icons">menu
                 </span>
